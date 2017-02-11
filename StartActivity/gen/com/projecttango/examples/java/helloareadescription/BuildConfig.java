aacea43e33d968f792d1c3dcc6940c9eeb753e6a
Indus Admin 1 <pavoindus@outlook.com>
2017-02-11 23:19:18 +0530

    Revert "Test Projects Uploaded"
    This reverts commit 567df8dbd62b83f13befea46a3d45f5915c99e0b.

--- StartActivity/gen/com/projecttango/examples/java/helloareadescription/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.projecttango.examples.java.helloareadescription;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
