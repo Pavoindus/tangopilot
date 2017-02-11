567df8dbd62b83f13befea46a3d45f5915c99e0b
Indus Admin 1 <pavoindus@outlook.com>
2017-02-11 23:00:10 +0530

    Test Projects Uploaded

---
+++ StartActivity/gen/com/projecttango/examples/java/helloareadescription/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.projecttango.examples.java.helloareadescription;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
